4a3c10ce44fd4d2cc2864ff48db34b78dda3798f
unclejamal <unclejamal@gmail.com>
2016-07-30 18:52:26 +0100

    Extract the test framework into a separate file

---
+++ site/public/js/pawel-test-framework.js
@@ -0,0 +1,49 @@
+// TEST FRAMEWORK
+function Assert() {
+};
+Assert.assertEquals = function(expected, actual) {
+  if (expected != actual) { throw "Expected [" + expected + "] but was [" + actual + "]" };
+};
+
+function TestSuiteResults() {
+  this.failures = {};
+
+  this.addFailure= function(testName, errorMessage) {
+    this.failures[testName] = errorMessage;
+  };
+
+  this.present = function() {
+    if (Object.keys(this.failures).length != 0) {
+      $(".site-wrapper").css("background-color", "#880e0e");
+      $(".site-wrapper").empty();
+
+      var that = this;
+      Object.keys(this.failures).forEach(function(testName) {
+        var failure = "[FAILURE] " + testName + " : " + that.failures[testName];
+        $(".site-wrapper").append("<h2>" + failure + "</h2>");
+      });
+    }
+  };
+};
+
+function TestSuite() {
+  this.tests = {};
+
+  this.addTest = function(testTitle, testFunction) {
+    this.tests[testTitle] = testFunction;
+  };
+
+  this.run = function() {
+    var that = this;
+    var testSuiteResults = new TestSuiteResults();
+    Object.keys(this.tests).forEach(function(testName) {
+      try {
+        that.tests[testName](testSuiteResults);
+      } catch (errorMessage) {
+        testSuiteResults.addFailure(testName, errorMessage);
+      };
+    });
+
+    testSuiteResults.present();
+  }
+};
